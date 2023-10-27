3bd648e97d8716a706ee58449abc53a44e7a3451
Perkq <artsynek@gmail.com>
2023-10-27 21:57:16 +0300

    Added 4 simple tests for every method

--- lab7/CalculatorTests/CalcTests.cs
+++ lab7/CalculatorTests/CalcTests.cs
@@ -7,12 +7,36 @@ namespace CalculatorTests
     {
 
         [Test]
-        public void Test1()
+        public void Sum_Test_EqualTo_ExpectedNumber()
         {
-            double x = 10.3;
+            double expected = 10.3;
 
-            double res = Calculator.Add(5.2, 5.1);
-            Assert.That(x, Is.EqualTo(res));
+            double result = Calculator.Add(5.2, 5.1);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Sub_Test_EqualTo_ExpectedNumber()
+        {
+            double expected = 30.8;
+            double result = Calculator.Sub(50.5, 19.7);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Mul_Test_EqualTo_ExpectedNumber()
+        {
+            double expected = 3.7 * 8.3;
+            double result = Calculator.Mul(3.7, 8.3);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Div_Test_EqualTo_ExpectedNumber()
+        {
+            double expected = 123.5 / 3.8;
+            double result = Calculator.Div(123.5, 3.8);
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
