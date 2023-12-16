04c1c46ac7d177d6e7881b617ec17ec1a1e640f6
Perkq <artsynek@gmail.com>
2023-12-16 03:49:31 +0300

    lab11 done

---
+++ Lab11/SeleniumTests/Driver/DriverInstance.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Firefox;
+using System.Diagnostics;
+using OpenQA.Selenium.Chrome;
+
+namespace Lab11.Driver
+{
+    public class DriverInstance
+    {
+        private static IWebDriver driver;
+
+        private DriverInstance() { }
+
+        public static IWebDriver GetInstance()
+        { 
+            driver = new ChromeDriver();
+   
+            return driver;
+        }
+
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ Lab11/SeleniumTests/Pages/BasketPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.PageObjects;
+
+namespace Lab11.Pages
+{
+    public class BasketPage
+    {
+        private const string BASE_URL = "https://wdl.by/basket/";
+
+        [FindsBy(How = How.ClassName, Using = "cart-item")]
+        private readonly IList<IWebElement> _basketItems;
+
+        public IWebDriver _driver;
+
+        public BasketPage(IWebDriver driver)
+        {
+            Thread.Sleep(2000);
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void OpenPage()
+        {
+            _driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        public int GetBasketItemsCount()
+        {
+            return _basketItems.Count;
+        }
+    }
+}
